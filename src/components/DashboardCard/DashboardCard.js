be421d7f681cb7b9f99a06c42716d1786ff9e73d
mbohorquez22 <miguel.lanzcom@gmail.com>
2022-07-18 17:42:56 -0500

    adición de columna dashboard

--- src/components/DashboardCard/DashboardCard.js
+++ src/components/DashboardCard/DashboardCard.js
@@ -8,6 +8,10 @@ export function DashboardCard (){
             <h2 className="card-title">Información general de App</h2>
             <h2 className="card-count-users">250.543</h2>
             <p className="card-subtitle">Usuarios</p>
+            <button className="btn-analytics">
+                <ion-icon name="analytics"></ion-icon>
+            </button>
         </div>
+        
     )
 }
